3a6aaa76f2ebd7f37992a1dabe82ea87ec2a07a9
Gordon Campbell <gordy2007@hotmail.co.uk>
2025-10-15 08:10:04 +0100

    cockpit added

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux cockpit cockpit-bridge cockpit-machines cockpit-networkmanager cockpit-ostree cockpit-podman cockpit-selinux cockpit-storaged cockpit-system  
 
 # Use a COPR Example:
 #
