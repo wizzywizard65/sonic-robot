21f22a089612ab3f982484d7df5ba96a0737770e
Gordon Campbell <gordy2007@hotmail.co.uk>
2025-10-21 21:51:32 +0100

    silverblue 43

--- Containerfile
+++ Containerfile
@@ -3,7 +3,7 @@ FROM scratch AS ctx
 COPY build_files /
 
 # Base Image
-FROM ghcr.io/ublue-os/bluefin-dx:latest
+FROM quay.io/fedora/fedora-silverblue:43
 COPY system_files /
 RUN mkdir -p /var/roothome
 
--- build_files/build.sh
+++ build_files/build.sh
@@ -11,17 +11,17 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 install -y btop cockpit cockpit-bridge cockpit-machines cockpit-networkmanager cockpit-ostree cockpit-podman cockpit-selinux cockpit-storaged cockpit-system  
-#dnf5 install -y podman-machine podman-compose podman-tui podmansh docker-buildx-plugin docker-ce docker-ce-cli docker-compose-plugin docker-model-plugin flatpak-builder distrobox virt-manager
-#dnf5 install -y jetbrains-mono-fonts-all gnome-tweaks btop code neovim bash-completion bcc-tools strace tmate
+dnf5 install -y podman-machine podman-compose podman-tui podmansh docker-buildx-plugin docker-ce docker-ce-cli docker-compose-plugin docker-model-plugin flatpak-builder distrobox virt-manager
+dnf5 install -y jetbrains-mono-fonts-all gnome-tweaks btop code neovim bash-completion bcc-tools strace tmate
 
 dnf5 -y clean all
 #Gnome extensions
 
-#dnf5 install -y gnome-shell-extension-dash-to-dock gnome-shell-extension-caffeine
+dnf5 install -y gnome-shell-extension-dash-to-dock gnome-shell-extension-caffeine
 
 
 #dnf5 install -y fedora-release-ostree-desktop
-#dnf5 -y clean all
+dnf5 -y clean all
 #remove firefox
 
 dnf5 -y remove firefox
@@ -33,12 +33,12 @@ dnf5 -y remove firefox
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-#dnf5 -y copr enable gmaglione/podman-bootc
-#dnf5 -y install podman-bootc
-#dnf5 -y copr disable gmaglione/podman-bootc
-#dnf5 -y copr enable ublue-os/packages
-#dnf5 -y install uupd
-#dnf5 -y copr disable ublue-os/packages
+dnf5 -y copr enable gmaglione/podman-bootc
+dnf5 -y install podman-bootc
+dnf5 -y copr disable gmaglione/podman-bootc
+dnf5 -y copr enable ublue-os/packages
+dnf5 -y install uupd
+dnf5 -y copr disable ublue-os/packages
 
 #### Example for enabling a System Unit File
 
