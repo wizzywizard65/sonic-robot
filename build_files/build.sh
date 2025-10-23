9f24079a2ee19ec3122b4de784943b06871d595a
Gordon Campbell <gordy2007@hotmail.co.uk>
2025-10-23 16:58:38 +0100

    added more codecs

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,10 +9,26 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+dnf5 install -y \
+        https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+        https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
 # this installs a package from fedora repos
 dnf5 install -y btop cockpit cockpit-bridge cockpit-machines cockpit-networkmanager cockpit-ostree cockpit-podman cockpit-selinux cockpit-storaged cockpit-system  
 dnf5 install -y podman-machine podman-compose podman-tui podmansh docker-buildx-plugin docker-ce docker-ce-cli docker-compose-plugin docker-model-plugin flatpak-builder distrobox virt-manager
 dnf5 install -y jetbrains-mono-fonts-all gnome-tweaks btop code neovim bash-completion bcc-tools strace tmate
+
+dnf5 install -y --allowerasing \
+    ffmpeg \
+    ffmpegthumbnailer \
+    gstreamer1-vaapi \
+    libavcodec-freeworld
+
+dnf5 install -y \
+    gstreamer1-plugin-openh264 \
+    libva-intel-driver \
+    mozilla-openh264
 
 dnf5 -y clean all
 #Gnome extensions
